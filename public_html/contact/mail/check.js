33dd2b722209af6383005bbd697f4cc7b19e04fb
Koji Kawasaki <kojikawasaki@kawasakikouji-no-MacBook-Pro.local>
2017-08-18 15:16:01 +0900

    リニューアル テスト

--- public_html/contact/mail/check.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-	new Spry.Widget.ValidationTextField("checkText1", "none", {validateOn:["change"]});
-	new Spry.Widget.ValidationTextField("checkText2", "email",{validateOn:["change"]});
-}
